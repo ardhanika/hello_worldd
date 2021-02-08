43b0cb4dee4c438aa809b89b4494989e2dccb609
= <=>
2021-02-08 23:08:23 +0700

    tugas1

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Tugas Pertama Flutter'),
     );
   }
 }
@@ -94,11 +94,10 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'Ardhanika Muji Rahman Hidayatullah',
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              '1931710165',
             ),
           ],
         ),
